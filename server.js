59b1a3a7b498b59978c25c1de7bb801050462012
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2020-05-13 22:03:26 +0100

    Add seat refreshening

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const testimonials = require('./routes/testimonials.routes');
 const cors = require('cors')
 const concerts = require('./routes/concerts.routes');
 const seats = require('./routes/seats.routes');
+const path = require('path');
 
 const app = express();
 
@@ -14,11 +15,17 @@ app.use('/api', testimonials);
 app.use('/api', concerts);
 app.use('/api', seats);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).json({message: '404 not found...'});
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
