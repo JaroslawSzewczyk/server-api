a0242b7d28e9bcce64bda30fb4d5f59c7d83babd
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2020-05-19 18:05:49 +0100

    Add WebSocket

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import { Button, Progress, Alert } from 'reactstrap';
+import io from 'socket.io-client';
 
 import './SeatChooser.scss';
 
 class SeatChooser extends React.Component {
   
   componentDidMount() {
-    const { loadSeats } = this.props;
-    let myInterval = setInterval(loadSeats, 2000);
-    this.setState({ myInterval: myInterval, });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.setState.myInterval);
+    const { loadSeats, loadSeatsData } = this.props;
+    loadSeats();
+    this.socket = io.connect('http://localhost:8000/');
+    this.socket.on('seatsUpdate', (seats) => loadSeatsData(seats));
   }
 
   isTaken = (seatId) => {
     const { seats, chosenDay } = this.props;
-
+    
     return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
   }
 
   prepareSeat = (seatId) => {
     const { chosenSeat, updateSeat } = this.props;
     const { isTaken } = this;
-
+    
     if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
     else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
     else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
@@ -33,7 +31,9 @@ class SeatChooser extends React.Component {
   render() {
 
     const { prepareSeat } = this;
-    const { requests } = this.props;
+    const { requests, seats } = this.props;
+
+    const seatsAmount = seats.filter(seat => seat.seat !== undefined);
 
     return (
       <div>
@@ -43,6 +43,7 @@ class SeatChooser extends React.Component {
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+        <p>Free seats: {seatsAmount.length}/50</p>
       </div>
     )
   };
--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const cors = require('cors')
 const concerts = require('./routes/concerts.routes');
 const seats = require('./routes/seats.routes');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 app.use('/api', testimonials);
 app.use('/api', concerts);
@@ -26,6 +32,14 @@ app.use((req, res) => {
   res.status(404).json({message: '404 not found...'});
 })
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New socket!');
+});
+
+
